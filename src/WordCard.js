4ec7e1e1647235cd3e760b81aee02a4ec643a46b
NATTHAPHONG.P <6110110038@psu.ac.th>
2023-07-31 15:29:59 +0700

    Part7-4

--- src/WordCard.js
+++ src/WordCard.js
@@ -7,6 +7,10 @@ const TIME_LIMIT = 30;
 const MAX_ATTEMPTS = 3;
 const MAX_SCORE = 100;
 
+const saveHighScores = (highScores) => {
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+};
+
 const prepareStateFromWord = (given_word) => {
   let word = given_word.toUpperCase();
   let chars = _.shuffle(Array.from(word));
@@ -23,19 +27,23 @@ const prepareStateFromWord = (given_word) => {
 
 export default function WordCard(props) {
   const [state, setState] = useState(prepareStateFromWord(props.value));
+  const [highScores, setHighScores] = useState([]);
+
+  useEffect(() => {
+    const savedHighScores = JSON.parse(localStorage.getItem('highScores') || '[]');
+    setHighScores(savedHighScores);
+  }, []);
 
   useEffect(() => {
     if (!state.completed && !state.gameOver) {
       const timer = setTimeout(() => {
         const newScore = Math.max(state.score - 1, 0);
         if (newScore === 0) {
-          // จับเวลา
           setState((prevState) => ({
             ...prevState,
             gameOver: true,
           }));
         } else {
-          // อัปเดตคะแแนนตามเวลา
           setState((prevState) => ({
             ...prevState,
             score: newScore,
@@ -46,6 +54,14 @@ export default function WordCard(props) {
       return () => clearTimeout(timer);
     }
   }, [state]);
+
+  const updateHighScores = (score) => {
+    const newHighScores = [...highScores, score];
+    newHighScores.sort((a, b) => b - a); // เรียงลำดับมาก>น้อย
+    const topHighScores = newHighScores.slice(0, 5); // เก็บค่าคะแนน 5 อันดับ
+    setHighScores(topHighScores);
+    saveHighScores(topHighScores);
+  };
 
   const activationHandler = (c) => {
     console.log(`${c} has been activated`);
@@ -61,6 +77,7 @@ export default function WordCard(props) {
           const bonusScore = MAX_SCORE * attemptsLeft;
           const totalScore = ((state.score * 10) + bonusScore) / 6;
           setState({ ...state, completed: true, score: totalScore });
+          updateHighScores(totalScore);
         } else {
           const nextAttempt = state.attempt + 1;
           if (nextAttempt > MAX_ATTEMPTS) {
@@ -92,14 +109,14 @@ export default function WordCard(props) {
       {state.completed && (
         <div className="congrats-message">
           <p>ยินดีด้วย คุณทายคำถูกต้อง</p>
-          <p>Score: {state.score.toFixed(2)}</p> {/* Display the score as a decimal */}
-          <button onClick={resetGame}>เล่นอีกครั้ง</button>
+          <p>Score: {state.score.toFixed(2)}</p>
+          <button onClick={resetGame}>Play Again</button>
         </div>
       )}
       {state.gameOver && (
         <div className="game-over-message">
           <p>คุณแพ้แล้ว</p>
-          <p>Final Score: {0}</p> {/* Display the score as a decimal */}
+          <p>Score: {0}</p>
           <button onClick={resetGame}>ลองใหม่</button>
         </div>
       )}
@@ -108,6 +125,14 @@ export default function WordCard(props) {
           <p>Time Remaining: {state.score} seconds</p>
         </div>
       )}
+      <div className="high-scores">
+        <h2>คะแนนที่ดีที่สุด</h2>
+        <ol>
+          {highScores.map((score, index) => (
+            <li key={index}>{score.toFixed(2)}</li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
