1f184a371250b06700f099ebbce6f1d6fc70eb69
NATTHAPHONG.P <6110110038@psu.ac.th>
2023-07-31 13:18:15 +0700

    Part7-2

--- src/WordCard.js
+++ src/WordCard.js
@@ -4,44 +4,59 @@ import _ from 'lodash';
 import './App.css'; // Create a CSS file to apply the styles.
 
 const prepareStateFromWord = (given_word) => {
-    let word = given_word.toUpperCase();
-    let chars = _.shuffle(Array.from(word));
-    return {
-        word,
-        chars,
-        attempt: 1,
-        guess: '',
-        completed: false,
-      };
+  let word = given_word.toUpperCase();
+  let chars = _.shuffle(Array.from(word));
+  return {
+    word,
+    chars,
+    attempt: 1,
+    guess: '',
+    completed: false,
+  };
 };
 
 export default function WordCard(props) {
-    const [state, setState] = useState(prepareStateFromWord(props.value));
+  const [state, setState] = useState(prepareStateFromWord(props.value));
 
-    const activationHandler = (c) => {
-        console.log(`${c} has been activated`)
+  const activationHandler = (c) => {
+    console.log(`${c} has been activated`);
 
-        let guess = state.guess + c
-        setState({...state, guess})
+    if (!state.completed) {
+      const guess = state.guess + c;
+      setState({ ...state, guess });
 
-        if(guess.length === state.word.length) {
-            if(guess === state.word) {
-                console.log('yeah!')
-                setState({...state, completed: true})
-            } else {
-                console.log('reset, next attemp')
-                setState({...state, guess: '', attempt: state.attempt + 1})
-            }
+      if (guess.length === state.word.length) {
+        if (guess === state.word) {
+          console.log('yeah!');
+          setState({ ...state, completed: true });
+        } else {
+          console.log('reset, next attempt');
+          setState({ ...state, guess: '', attempt: state.attempt + 1 });
         }
-    };
+      }
+    }
+  };
 
-    return (
-        <div className="word-card">
-            {    
-                state.chars.map((c, i) => (
-                    <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>
-                )
-            )}
+  const resetGame = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className="word-card">
+      {state.chars.map((c, i) => (
+        <CharacterCard
+          value={c}
+          key={i}
+          activationHandler={activationHandler}
+          attempt={state.attempt}
+        />
+      ))}
+      {state.completed && (
+        <div className="congrats-message">
+          <p>ขอแสดงความยินดี คุณทายถูก</p>
+          <button onClick={resetGame}>Play Again</button>
         </div>
-    );
+      )}
+    </div>
+  );
 }
