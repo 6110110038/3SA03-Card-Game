6b90cdf4a6dc305d091b711498a8db3e96fe6b6c
NATTHAPHONG.P <6110110038@psu.ac.th>
2023-07-31 10:25:10 +0700

    Part6-fix

--- src/WordCard.js
+++ src/WordCard.js
@@ -3,46 +3,44 @@ import CharacterCard from './CharacterCard';
 import _ from 'lodash';
 
 const prepareStateFromWord = (given_word) => {
-	let word = given_word.toUpperCase()
-	let chars = _.shuffle(Array.from(word))
-	return {
-		word,
-		chars,
-		attempt: 1,
-		guess: '',
-		completed: false
-	}
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: '',
+        completed: false
+    }
 }
 
-export default function WordCard(props){
+export default function WordCard(props) {
 
-const [state, setState] = useState(prepareStateFromWord(props.value))
+    const [state, setState] = useState(prepareStateFromWord(props.value))
 
-	const activationHandler = c => {
-		console.log(`${c} has been activated.`)
-		let guess = state.guess + c
-		setState({...state, guess})
+    const activationHandler = c => {
+        console.log(`${c} has been activated`)
 
-		if(guess.length === state.word.length){ 
-			if(guess === state.word){
-				console.log('yeah!')
-				setState({...state, guess: '', completed: true})
-			}
-			else{
-				console.log('reset')
-				setState({...state, guess: '', attempt: state.attempt + 1})
-				}
-			}
-		//console.log(guess)
-	}
+        let guess = state.guess + c
+        setState({...state, guess})
 
-	return (
-		<div>
-			{
+        if(guess.length == state.word.length) {
+            if(guess == state.word) {
+                console.log('yeah!')
+                setState({...state, completed: true})
+            } else {
+                console.log('reset, next attemp')
+                setState({...state, guess: '', attempt: state.attempt + 1})
+            }
+        }
+    }
+
+    return (
+        <div>
+            {
                 state.chars.map((c, i) =>
-					<CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>
-				)
-			}
-		</div>
-	);
+                    <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>)
+            }
+        </div>
+    );
 }
